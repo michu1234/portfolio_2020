7cdc91fc57be2ff06a414170ab01300424951531
“michu1234” <dawiddeveloper@gmail.com>
2019-09-29 00:40:42 +0200

    scaffold

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/works">Works</router-link>
       <router-link to="/about">About</router-link>
+      <p>test</p>
     </div>
     <router-view/>
   </div>
